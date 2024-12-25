110a952b36bb38e791df82fb0c194bcf6b3652cd
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2024-12-24 23:09:18 -0800

    Moved Home index page

---
+++ website/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+        <>
+            <main>
+                <div className="incontainer">
+                    <h1>Dine Digest</h1>
+                </div>
+            </main>
+        </>
+    )
+};
+
+export default Home;
