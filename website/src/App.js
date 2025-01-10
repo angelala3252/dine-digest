47d9346eea06780af0149e0d805258ed9d665952
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2025-01-09 21:14:07 -0500

    Add home page, routing, and styling

--- website/src/App.js
+++ website/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Layout from './components/Layout';
 
 function App() {
   return <BrowserRouter>
     <Routes>
-      <Route path="/">
-        <Route index element={<Home />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Navigate to="home" />} />
+        <Route path='home' element={<Home />}/>
       </Route>
     </Routes>
   </BrowserRouter>;
--- website/src/pages/Home/index.jsx
+++ website/src/pages/Home/index.jsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
+import '../shared.css';
+
 const Home = () => {
     return (
         <>
             <main>
                 <div className="incontainer">
-                    <h1>Dine Digest</h1>
+                    <h1>Your 2024 Food Recap</h1>
+                    <Link>Meals</Link>
+                    <Link>Desserts</Link>
                 </div>
             </main>
         </>
